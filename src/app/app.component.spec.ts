ebd374e42eedea21fff80d9b9cb69c7d990d21a7
carolinaolv <carolinaolatevallejos96@gmail.com>
2019-06-19 15:02:35 -0400

    corrigiendo test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -33,6 +33,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Burger-queen!');
+    expect(compiled.querySelector('h1').textContent).toContain('Burger-queen');
   });
 });
